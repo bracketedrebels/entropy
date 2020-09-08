957bd87b018e89be2a60c3025ecc9a537b2e024a
Dan Mironenko <wolfson@bracketedrebels.com>
2020-09-08 11:22:37 +0300

    feat: trimmer starting to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
-function App() {
+import { Vector2, sum, sub, max } from "./utils/vector";
+import { uniqueId } from "lodash";
+import { useDrag } from "react-use-gesture";
+import { Trimmer } from "./components/trimmer";
+import { useSize } from "./components/measured";
+
+const data = new Array(1)
+  .fill(undefined)
+  .map(() => ({ id: uniqueId("item"), position: Vector2.random(500) }));
+
+const App = () => {
+  const ref = useRef(null);
+  const size = useSize(ref);
+  const [finder, setFinder] = useState([Vector2.zero, Vector2.zero] as [
+    Vector2,
+    Vector2
+  ]);
+  useEffect(
+    () => setFinder((prev) => [prev[0], sum(prev[0], [size[0], size[1]])]),
+    [size]
+  );
+
+  const bindDrag = useDrag(({ delta }) => {
+    setFinder((prev) => {
+      const tl = max(sub(prev[0], delta), Vector2.zero);
+      const normalizedDelta = sub(tl, prev[0]);
+      const br = sum(prev[1], normalizedDelta);
+      return [tl, br];
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div {...bindDrag()} ref={ref} className="fixed inset-0 select-none">
+      <Trimmer data={data} finder={finder} className="w-full h-full">
+        {({ item: { id } }) => (
+          <div className="border-solid border border-gray-400 bg-gray-200 p-4 m-0 font-sans font-thin">{`id: ${id}`}</div>
+        )}
+      </Trimmer>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/trimmer.tsx
@@ -0,0 +1,197 @@
+import React, {
+  useState,
+  useEffect,
+  HTMLAttributes,
+  useCallback,
+  useRef,
+  useMemo,
+} from "react";
+import { identity, Omit, uniqueId, differenceBy, entries } from "lodash";
+import { useSize, Measured } from "./measured";
+import { Vector2, sub } from "../utils/vector";
+import { Collection } from "lokijs";
+
+export type Trimmer<T extends { id: string; position: Vector2 }> = Omit<
+  HTMLAttributes<HTMLDivElement>,
+  "ref"
+> & {
+  finder?: [Vector2, Vector2];
+  data?: T[];
+  children?: (payload: { item: { id: string }; scale: Vector2 }) => JSX.Element;
+};
+export const Trimmer = <T extends { position: Vector2; id: string }>({
+  data = [],
+  finder = [Vector2.zero, Vector2.zero],
+  children = identity,
+  ...props
+}: Trimmer<T>) => {
+  const [renderables, { read, update, insert }] = useRectangles();
+  useEffect(
+    () =>
+      void insert(
+        data.map((v) => ({
+          id: v.id,
+          top: v.position[1],
+          right: Number.POSITIVE_INFINITY,
+          bottom: Number.POSITIVE_INFINITY,
+          left: v.position[0],
+        }))
+      ),
+    []
+  );
+  useEffect(() => {
+    const [[left, top], [right, bottom]] = finder;
+    const finderNotEmpty = left < right && top < bottom;
+    if (finderNotEmpty) {
+      read({
+        $or: [
+          {
+            left: { $lte: left },
+            top: { $lte: top },
+            right: { $gte: right },
+            bottom: { $gte: bottom },
+          },
+          {
+            left: { $lte: left },
+            top: { $lte: top },
+            right: { $gte: left },
+            bottom: { $gte: top },
+          },
+          {
+            left: { $lte: right },
+            top: { $lte: bottom },
+            right: { $gte: right },
+            bottom: { $gte: bottom },
+          },
+          {
+            left: { $gte: left },
+            top: { $gte: top },
+            right: { $lte: right },
+            bottom: { $lte: bottom },
+          },
+        ],
+      });
+    }
+  }, [finder]);
+
+  const ref = useRef(null);
+  const size = useSize(ref);
+
+  const [scale, setScale] = useState(Vector2.one);
+  useEffect(
+    () =>
+      setScale(
+        (([w, h]) => [w / size[0], h / size[1]] as Vector2)(
+          sub(finder[1], finder[0])
+        )
+      ),
+    [size, finder]
+  );
+
+  const [translate, setTranslate] = useState(Vector2.zero);
+  useEffect(() => setTranslate(finder[0]), [finder]);
+
+  const [transform, setTransform] = useState("");
+  useEffect(
+    () =>
+      setTransform(
+        `translate(-${translate[0]}px, -${translate[1]}px) scale(${scale[0]}, ${scale[1]})`
+      ),
+    [translate, scale]
+  );
+
+  useEffect(() => console.log(renderables), [renderables]);
+
+  return (
+    <div {...props} ref={ref}>
+      <div style={{ transform }}>
+        {renderables.map((v) => (
+          <Measured
+            key={v.id}
+            style={{
+              top: v.top,
+              left: v.left,
+            }}
+            className="absolute"
+            dimensionsChanged={([w, h]) => {
+              update([{ ...v, bottom: v.top + h, right: v.left + w }]);
+            }}
+          >
+            <h3>{children({ item: v, scale })}</h3>
+          </Measured>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const useRectangles = <
+  T extends {
+    id: string;
+    top: number;
+    right?: number;
+    bottom?: number;
+    left: number;
+  }
+>() => {
+  const [data, setData] = useState<T[]>([]);
+  const [query, setQuery] = useState<LokiQuery<T>>();
+  const [view, setView] = useState<DynamicView<T>>();
+  const collection = useMemo(
+    () =>
+      new Collection<T>(uniqueId(), {
+        asyncListeners: true,
+        disableMeta: true,
+        indices: ["top", "right", "bottom", "left"],
+        unique: ["id"],
+      }),
+    []
+  );
+
+  const insert = useCallback((payload: T[]) => collection.insert(payload), [
+    collection,
+  ]);
+  const update = useCallback((payload: T[]) => collection.update(payload), [
+    collection,
+  ]);
+  const remove = useCallback((payload: T[]) => collection.remove(payload), [
+    collection,
+  ]);
+
+  useEffect(() => {
+    const uid = uniqueId();
+    setView(collection.addDynamicView(uid));
+    return () => void collection.removeDynamicView(uid);
+  }, [collection]);
+
+  useEffect(() => {
+    if (!!view && !!query) {
+      const uid = uniqueId();
+      view.applyFilter({
+        type: "find",
+        val: query,
+        uid,
+      });
+      const listener = () =>
+        setData((olddata) => {
+          const newdata = view.data();
+          const diff = differenceBy(newdata, olddata, (v) =>
+            entries(v).sort().join("::")
+          );
+          return diff.length === 0
+            ? olddata
+            : [
+                ...olddata.filter(({ id }) => !diff.find((v) => v.id === id)),
+                ...diff,
+              ];
+        });
+      view.addListener("rebuild", listener);
+      return () => {
+        view.removeFilter(uid);
+        view.removeListener("rebuild", listener);
+      };
+    }
+  }, [view, query]);
+
+  return [data, { read: setQuery, insert, update, remove }] as const;
+};
