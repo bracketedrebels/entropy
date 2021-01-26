f63aee707f839391ec7c8e3dd307b9a152edc23d
wolfson@bracketedrebels.com <wolfson@bracketedrebels.com>
2021-01-26 17:04:10 +0700

    chore: new webpack configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,40 @@
 import React, { Suspense, lazy } from "react";
 import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
-import { hot } from "react-hot-loader/root";
 // routing components...
 const Welcome = lazy(() => import("./components/+welcome"));
 const Editor = lazy(() => import("./components/+editor"));
 // ...routing components
 
-export default hot(
-  (
-    props: React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLDivElement>,
-      HTMLDivElement
-    >
-  ) => (
-    <div
-      {...props}
-      style={
-        {
-          "--neumorphic-light-azimuth": 45,
-          "--neumorphic-shadow": "rgba(0, 0, 0, .3)",
-          "--neumorphic-light": "rgba(240, 240, 255, 1)",
-          "--glass-bevel": "2px",
-        } as React.CSSProperties
-      }
-    >
-      <div className="absolute inset-0 flex items-stretch justify-stretch bg-surface transition-all duration-200">
-        <div className="absolute inset-0 pointer-events-none neu-convex" />
-        <div className="absolute inset-0 bg-repeat-round opacity-25 pointer-events-none" />
+export default (
+  props: React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >
+) => (
+  <div
+    {...props}
+    style={
+      {
+        "--neumorphic-light-azimuth": 45,
+        "--neumorphic-shadow": "rgba(0, 0, 0, .3)",
+        "--neumorphic-light": "rgba(240, 240, 255, 1)",
+        "--glass-bevel": "2px",
+      } as React.CSSProperties
+    }
+  >
+    <div className="absolute inset-0 flex items-stretch justify-stretch bg-surface transition-all duration-200">
+      <div className="absolute inset-0 pointer-events-none neu-convex" />
+      <div className="absolute inset-0 bg-repeat-round opacity-25 pointer-events-none" />
 
-        <BrowserRouter basename={process.env.ROUTING_BASENAME}>
-          <Suspense
-            fallback={<div className="m-auto text-shadow">Loading...</div>}
-          >
-            <Switch>
-              <Route exact path="/welcome" component={Welcome} />
-              <Route exact path="/editor" component={Editor} />
-              <Redirect to="/welcome" />
-            </Switch>
-          </Suspense>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter basename={process.env.ROUTING_BASENAME}>
+        <Suspense fallback={<div className="m-auto text-shadow">Loading</div>}>
+          <Switch>
+            <Route exact path="/welcome" component={Welcome} />
+            <Route exact path="/editor" component={Editor} />
+            <Redirect to="/welcome" />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
     </div>
-  )
+  </div>
 );
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,71 +1,211 @@
-import path from "path";
-import webpack from "webpack";
-import Dotenv from "dotenv-webpack";
+import path, { sep } from "path";
 import fs from "fs";
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import { append, concat, identity } from "ramda";
 
-module.exports = ({ environment = "dev" }: any) => {
-  const basePath = `${path.join(__dirname)}${path.sep}.env`;
+import Dotenv from "dotenv-webpack";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
+import postcss from "postcss";
+import handlebars from "handlebars";
+
+import type { Configuration } from "webpack";
+import { DefinePlugin } from "webpack";
+
+/* helpers definitions */
+
+const rule = (
+  v: (
+    env: Environment
+  ) => (
+    acc: Required<Required<Configuration>["module"]>["rules"]
+  ) => Required<Required<Configuration>["module"]>["rules"]
+) => (env: Environment, conf: Configuration) =>
+  ({
+    ...conf,
+    module: {
+      ...(conf.module || {}),
+      rules: v(env)(conf.module?.rules || []),
+    },
+  } as Configuration);
+
+const config = (
+  ...args: Array<
+    (env: Required<Environment>, config: Configuration) => Configuration
+  >
+) => ({
+  environment = "dev",
+  output = "dist",
+  basename = "/",
+  bundlename = "bundle.js",
+}: Environment) =>
+  args.reduce(
+    (acc, v) =>
+      v(
+        {
+          environment,
+          output: path.resolve(__dirname, output),
+          basename,
+          bundlename,
+        },
+        acc
+      ),
+    {
+      entry: ["./src/index.tsx"],
+      devServer: {
+        historyApiFallback: true,
+        compress: true,
+        hot: true,
+      },
+      optimization: {
+        usedExports: environment !== "dev",
+        minimize: environment !== "dev",
+      },
+      output: {
+        path: path.resolve(__dirname, output),
+        filename: bundlename,
+      },
+      resolve: {
+        extensions: [".js", ".ts", ".tsx"],
+      },
+    } as Configuration
+  );
+
+const plugin = (
+  v: (
+    env: Required<Environment>
+  ) => (
+    conf: Required<Configuration>["plugins"]
+  ) => Required<Configuration>["plugins"]
+) => (env: Required<Environment>, conf: Configuration) =>
+  ({
+    ...conf,
+    plugins: v(env)(conf.plugins || []),
+  } as Configuration);
+
+const forceTypeChecking = () => () => append(new ForkTsCheckerWebpackPlugin());
+const defineRuntimeEnvironment = () => ({
+  environment,
+  basename,
+}: Required<Environment>) => {
+  const basePath = `${__dirname}${path.sep}.env`;
   const envPath = `${basePath}.${environment}`;
   const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+  return concat([
+    new Dotenv({
+      path: finalPath,
+      allowEmptyValues: true,
+    }),
+    new DefinePlugin({
+      "process.env.ROUTING_BASENAME": basename,
+    }),
+  ]);
+};
 
-  return {
-    entry: ["react-hot-loader/patch", "./src/index.tsx"],
-    devServer: {
-      historyApiFallback: true,
+const enableHMRForDevelopment = () => ({ environment }: Environment) =>
+  environment === "prod" ? identity : append(new ReactRefreshWebpackPlugin());
+
+const workerLoader = () => () =>
+  append({
+    test: /\.worker\.js$/,
+    use: {
+      loader: require.resolve("worker-loader"),
     },
-    output: {
-      filename: "bundle.js",
-    },
-    plugins: [
-      new ForkTsCheckerWebpackPlugin(),
-      new Dotenv({
-        path: finalPath,
-        allowEmptyValues: true,
-      }),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.worker\.js$/,
-          use: {
-            loader: "worker-loader",
-          },
-        },
-        {
-          test: /\.ts(x)?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/react",
-                "@babel/typescript",
-                ["@babel/env", { modules: false }],
-              ],
-              plugins: [
-                "relay",
-                "const-enum",
-                [
-                  "@babel/plugin-transform-typescript",
-                  { allowNamespaces: true },
-                ],
-              ],
-            },
-          },
-        },
-        {
-          test: /\.js$/,
-          use: ["source-map-loader"],
-          enforce: "pre",
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-      alias: {
-        "react-dom": "@hot-loader/react-dom",
+  });
+
+const tsxLoader = () => (env: Environment) =>
+  append({
+    test: /\.ts(x)?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [
+          "@babel/react",
+          "@babel/typescript",
+          ["@babel/env", { modules: false }],
+        ],
+        plugins: [
+          "relay",
+          "const-enum",
+          ["@babel/plugin-transform-typescript", { allowNamespaces: true }],
+          ...(env.environment === "dev"
+            ? [require.resolve("react-refresh/babel")]
+            : []),
+        ],
       },
     },
-  } as webpack.Configuration;
+  });
+
+const jsLoader = () => () =>
+  append({
+    test: /\.js$/,
+    use: {
+      loader: require.resolve("source-map-loader"),
+    },
+    enforce: "pre" as const,
+  });
+
+/**
+ * @todo
+ */
+const prepareAllTheStaticResources = () => ({
+  output,
+  environment,
+  basename,
+  bundlename,
+}: Environment) =>
+  concat([
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "static/index.css",
+          to: `${output}${sep}index.css`,
+          transform: (content) =>
+            postcss([
+              require("postcss-import"),
+              require("tailwindcss"),
+              require("autoprefixer"),
+              ...(environment === "prod"
+                ? [
+                    require("@fullhuman/postcss-purgecss")({
+                      content: ["./src/**/*.tsx", "./static/**/*.html"],
+                    }),
+                  ]
+                : []),
+            ])
+              .process(content, {
+                from: "static/index.css",
+                to: `${output}${sep}index.css`,
+              })
+              .then((v) => v.css),
+        },
+        {
+          from: "static/index.html",
+          to: `${output}${sep}index.html`,
+          transform: (content) =>
+            handlebars.compile(content.toString())({ basename, bundlename }),
+        },
+        { from: "static", to: output },
+      ],
+    }),
+  ]);
+
+type Environment = {
+  environment?: "dev" | "prod";
+  output?: string;
+  bundlename?: string;
+  basename?: string;
 };
+
+/* actual configuration */
+
+module.exports = config(
+  plugin(forceTypeChecking()),
+  plugin(defineRuntimeEnvironment()),
+  plugin(enableHMRForDevelopment()),
+  plugin(prepareAllTheStaticResources()),
+  rule(tsxLoader()),
+  rule(workerLoader()),
+  rule(jsLoader())
+);
